da39cc323d2f03d152cf84d9fd8889e46d5e0bd3
George Satellite <lunaerxs@gmail.com>
2024-01-27 17:54:37 +0200

    leva

--- src/components/Scene/Icosahedron/Icosahedron.tsx
+++ src/components/Scene/Icosahedron/Icosahedron.tsx
@@ -1,10 +1,21 @@
+import {useControls} from 'leva';
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 
 export function Icosahedron() {
+  const {detail} = useControls({
+    detail: {
+      value: 2,
+      min: 1,
+      max: 20,
+      step: 1,
+    },
+  });
+  const detailRounded = Math.round(detail);
+
   return (
     <mesh>
-      <icosahedronGeometry args={[1, 2]} />
+      <icosahedronGeometry args={[1, detailRounded]} />
       <shaderMaterial
         wireframe
         vertexShader={vertexShader}
