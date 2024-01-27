f5e3254b9ff68f0985539fb8ed7ffdb84781c49f
George Satellite <lunaerxs@gmail.com>
2024-01-27 14:19:25 +0200

    remove cube rotation code

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,15 +1,13 @@
 'use client';
-import {useEffect, useRef, useState} from 'react';
+import {useEffect, useRef} from 'react';
 import {Canvas, useFrame} from '@react-three/fiber';
 import {Environment, OrbitControls, PerspectiveCamera} from '@react-three/drei';
-import {lerp} from 'three/src/math/MathUtils.js';
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 
 export function Scene() {
   return (
     <Canvas>
-      <axesHelper />
       <SceneBody />
     </Canvas>
   );
@@ -17,8 +15,6 @@ export function Scene() {
 
 function SceneBody() {
   const cameraRef = useRef<React.ElementRef<typeof PerspectiveCamera>>(null);
-  const [inOrbitControlsInteraction, setInOrbitControlsInteraction] =
-    useState<boolean>(false);
 
   useEffect(() => {
     const camera = cameraRef.current;
@@ -37,50 +33,16 @@ function SceneBody() {
         minDistance={2}
         maxDistance={10}
         camera={cameraRef.current ?? undefined}
-        onStart={() => {
-          setInOrbitControlsInteraction(true);
-        }}
-        onEnd={() => {
-          setInOrbitControlsInteraction(false);
-        }}
       />
       <ambientLight />
-      <Cube rotationDisabled /* ={inOrbitControlsInteraction} */ />
+      <Cube />
     </>
   );
 }
 
-function Cube({rotationDisabled}: {readonly rotationDisabled: boolean}) {
-  const speed = 1.25;
-  const transitionSpeed = 2.5;
-  const currentSpeedRef = useRef<number>(rotationDisabled ? 0 : speed);
-  const targetSpeedRef = useRef<number>(rotationDisabled ? 0 : speed);
+function Cube() {
   const meshRef = useRef<React.ElementRef<'mesh'>>(null);
-
-  useEffect(() => {
-    targetSpeedRef.current = rotationDisabled ? 0 : speed;
-  }, [rotationDisabled]);
-
-  useFrame((_, deltaTime) => {
-    currentSpeedRef.current = lerp(
-      currentSpeedRef.current,
-      targetSpeedRef.current,
-      deltaTime * transitionSpeed,
-    );
-  });
-
-  useFrame((_, deltaTime) => {
-    const currentSpeed = currentSpeedRef.current;
-    const mesh = meshRef.current;
-    if (!mesh) return;
-
-    mesh.rotation.y += deltaTime * currentSpeed;
-  });
-
   const shaderMaterialRef = useRef<React.ElementRef<'shaderMaterial'>>(null);
-  useEffect(() => {
-    console.debug('shaderMaterialRef.current: ', shaderMaterialRef.current);
-  }, []);
 
   useFrame(({clock}) => {
     const shaderMaterial = shaderMaterialRef.current;
@@ -94,6 +56,7 @@ function Cube({rotationDisabled}: {readonly rotationDisabled: boolean}) {
       <boxGeometry args={[1, 1, 1, 4, 4, 4]} />
       <shaderMaterial
         ref={shaderMaterialRef}
+        wireframe
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
         uniforms={uniforms}
