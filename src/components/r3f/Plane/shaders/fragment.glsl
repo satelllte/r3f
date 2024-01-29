dd284208eb02a4879ed3ffc7537d66030c84126f
George Satellite <lunaerxs@gmail.com>
2024-01-29 21:15:57 +0200

    ./glsl_format.sh

--- src/components/r3f/Plane/shaders/fragment.glsl
+++ src/components/r3f/Plane/shaders/fragment.glsl
@@ -10,5 +10,6 @@ void main() {
     float blendX = step(0.95, 1.0 - abs(vUv.x - 0.5));
     float blendY = step(0.95, 1.0 - abs(vUv.y - 0.5));
 
-    gl_FragColor = vec4(mix(colorBlack, uColor, min(blendX + blendY, 1.0)), 1.0);
+    gl_FragColor =
+        vec4(mix(colorBlack, uColor, min(blendX + blendY, 1.0)), 1.0);
 }
