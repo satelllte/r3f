2c157777ab461161339bd4ea5fce7a84c529c495
George Satellite <lunaerxs@gmail.com>
2024-01-30 14:45:57 +0200

    load Can model

--- src/components/r3f/Cans/Cans.tsx
+++ src/components/r3f/Cans/Cans.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import {Environment} from '@react-three/drei';
 import {Can} from './Can';
 
 export function Cans() {
   return (
     <>
+      <Environment preset='dawn' />
       <Can position={[0, 0, 0]} />
       <Can position={[-1.25, 0, 0]} />
       <Can position={[1.25, 0, 0]} />
