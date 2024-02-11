43b1d412083bf58e9a07c3532dd00a59b27e4f54
Yehor M <20585619+satelllte@users.noreply.github.com>
2024-02-11 16:26:17 +0200

    base controls

--- src/components/r3f/Torus/Torus.tsx
+++ src/components/r3f/Torus/Torus.tsx
@@ -1,13 +1,41 @@
 'use client';
-import {useRef} from 'react';
-import {useFrame} from '@react-three/fiber';
+import {useControls} from 'leva';
 
 export function Torus() {
-  const meshRef = useRef<React.ElementRef<'mesh'>>(null);
+  const {
+    radialSegments: _radialSegments,
+    tubularSegments: _tubularSegments,
+    color,
+    wireframe,
+  } = useControls({
+    radialSegments: {
+      value: 12,
+      min: 3,
+      max: 36,
+      step: 1,
+    },
+    tubularSegments: {
+      value: 48,
+      min: 3,
+      max: 96,
+      step: 1,
+    },
+    color: {
+      value: '#7697c5',
+    },
+    wireframe: {
+      value: true,
+    },
+  });
+  const radialSegments = Math.round(_radialSegments);
+  const tubularSegments = Math.round(_tubularSegments);
+
   return (
-    <mesh ref={meshRef}>
-      <torusGeometry />
-      <meshStandardMaterial wireframe />
+    <mesh>
+      <torusGeometry
+        args={[undefined, undefined, radialSegments, tubularSegments]}
+      />
+      <meshStandardMaterial wireframe={wireframe} color={color} />
     </mesh>
   );
 }
