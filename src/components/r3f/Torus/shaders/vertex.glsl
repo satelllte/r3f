590e2a361120c97fc816473c2e06a01583f9bee9
Yehor M <20585619+satelllte@users.noreply.github.com>
2024-02-11 23:46:59 +0200

    glsl format

--- src/components/r3f/Torus/shaders/vertex.glsl
+++ src/components/r3f/Torus/shaders/vertex.glsl
@@ -6,30 +6,30 @@ varying float vNoiseMultiplier;
 uniform float uTime;
 
 vec3 random3(vec3 c) {
-    float j = 4096.0*sin(dot(c,vec3(17.0, 59.4, 15.0)));
+    float j = 4096.0 * sin(dot(c, vec3(17.0, 59.4, 15.0)));
     vec3 r;
-    r.z = fract(512.0*j);
+    r.z = fract(512.0 * j);
     j *= .125;
-    r.x = fract(512.0*j);
+    r.x = fract(512.0 * j);
     j *= .125;
-    r.y = fract(512.0*j);
-    return r-0.5;
+    r.y = fract(512.0 * j);
+    return r - 0.5;
 }
 
-const float F3 =  0.3333333;
-const float G3 =  0.1666667;
+const float F3 = 0.3333333;
+const float G3 = 0.1666667;
 float snoise(vec3 p) {
 
     vec3 s = floor(p + dot(p, vec3(F3)));
     vec3 x = p - s + dot(s, vec3(G3));
 
     vec3 e = step(vec3(0.0), x - x.yzx);
-    vec3 i1 = e*(1.0 - e.zxy);
-    vec3 i2 = 1.0 - e.zxy*(1.0 - e);
+    vec3 i1 = e * (1.0 - e.zxy);
+    vec3 i2 = 1.0 - e.zxy * (1.0 - e);
 
     vec3 x1 = x - i1 + G3;
-    vec3 x2 = x - i2 + 2.0*G3;
-    vec3 x3 = x - 1.0 + 3.0*G3;
+    vec3 x2 = x - i2 + 2.0 * G3;
+    vec3 x3 = x - 1.0 + 3.0 * G3;
 
     vec4 w, d;
 
@@ -58,7 +58,7 @@ void main() {
     vec2 _uv = uv;
 
     float noiseMultiplier = (abs(_uv.x - 0.5)) * 2.0;
-    float noise = snoise(_position * 2.0 + sin(uTime) * 0.5); 
+    float noise = snoise(_position * 2.0 + sin(uTime) * 0.5);
     float displacement = noise * noiseMultiplier;
     _position += _normal * 0.5 * displacement;
 
